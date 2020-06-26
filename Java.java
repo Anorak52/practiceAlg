4f4336e32b1767c0e29c193478319554e64cd714
Anorak52 <siber56@yandex.ru>
2020-06-26 14:19:15 +0300

    upd

--- Java.java
+++ Java.java
@@ -3,14 +3,12 @@ public class Java {
     public static void main(String[] args) {
         int[] mas = new int[] {1, 3, 6, 1, 0, 9};
         
-        for(int i = 0; i < mas.length-1; i++){
-            int min_index = i;
-            for(int j = i+1; j<mas.length-1; j++){
-                if (mas[j]<mas[min_index]) min_index = j;
+        for(int i = 1; i < mas.length; i++){
+            for(int j =i; j > 0 && mas[j-1] > mas[j]; j--){
+                int tmp = mas[j-1];
+                mas[j-1] = mas[j];
+                mas[j] = tmp;
             }
-            int tmp = mas[i];
-            mas[i] = mas[min_index];
-            mas[min_index] = tmp;
         }
 
         for(int i = 0; i < mas.length; i++){
