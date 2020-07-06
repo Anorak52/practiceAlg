baf6584446e0d4fd3835cbc90ffc8b9bebee3895
Anorak52 <siber56@yandex.ru>
2020-07-06 14:36:51 +0300

    Done

--- Java.java
+++ Java.java
@@ -1,18 +1,35 @@
-
 public class Java {
     public static void main(String[] args) {
-        int[] mas = new int[] {1, 3, 6, 1, 0, 9};
+        int[] array = new int[]{2, 5, 7, 1, 7, 3, 2};
         
-        for(int i = 1; i < mas.length; i++){
-            for(int j =i; j > 0 && mas[j-1] > mas[j]; j--){
-                int tmp = mas[j-1];
-                mas[j-1] = mas[j];
-                mas[j] = tmp;
+        int last = array.length-1;
+        QuickSort(array, 0, last);
+
+        for(int i =0; i < array.length; i++){System.out.println(array[i]);}
+    }
+
+    private static void QuickSort(int[] array, int startind, int lastind) {  //слева - больше, справа - меньше
+        if(startind >= lastind) return;  //если остался один элемент
+
+        int l = startind; //i
+        int r = lastind; //j
+
+        int mid = l - (l - r) / 2; 
+        
+        while (l < r){
+            while(l < mid && (array[l] <= array[mid])) l++;
+            while(r > mid && (array[mid] <= array[r])) r--;
+            
+            if(l < r){
+                int tmp = array[l];
+                array[l] = array[r];
+                array[r] = tmp;
+                if (l == mid) mid = r;
+                else if (r == mid) mid = l;
             }
         }
 
-        for(int i = 0; i < mas.length; i++){
-            System.out.println(mas[i]);
-        }
+        QuickSort(array, startind, mid); //если до r что то осталось
+        QuickSort(array, mid + 1, lastind); //если после l чет осталось
     }
 }
